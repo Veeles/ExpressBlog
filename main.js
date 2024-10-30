bf72ceeb84be49e465cae635d0039c05fef8b34e
Veeles <kacper.ros2@interia.pl>
2024-10-30 16:18:13 +0100

    Create save posts functionality

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-
+app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+const posts = []
+function addPost(title, description){
+    let index = posts.length
+    let newPost = {
+        'id': index + 1,
+        'title': title,
+        'description': description
+    }
+    posts.push(newPost)
+
+}
 
 app.get('/', (req,res) => {
     res.render('main.ejs');
+    console.log(posts)
 });
 
 app.get('/contact', (req, res) => {
@@ -17,6 +29,15 @@ app.get('/about', (req, res) => {
     res.render('about.ejs')
 })
 
+app.get('/add', (req, res) => {
+    res.render('add.ejs')
+})
+
+app.post('/add/post', (req, res) => {
+
+    addPost(req.body['Title'], req.body['text']);
+    res.redirect('/');
+});
 
 app.listen(port, (err) => {
     if (err) console.log(err);
